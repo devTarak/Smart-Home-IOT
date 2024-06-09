bf9540ca447eac7c90ee7535583f0220fc841777
Tarak Rahman <tarakrahman421@gmail.com>
2024-06-10 05:51:11 +0600

    added

---
+++ 2chrelaynodemcu.ino
@@ -0,0 +1,85 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  NOTE: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+//#define BLYNK_PRINT Serial
+
+/* Fill in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL6fj_9Sake"
+#define BLYNK_TEMPLATE_NAME "Smart Home"
+#define BLYNK_AUTH_TOKEN "JeDv6aLERppQtNRNYVyoD1aKl-zFlcKK"
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+//Define the relay pins
+#define relay1 D1
+#define relay2 D2
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Tarak";
+char pass[] = "00000000";
+BLYNK_WRITE(V1) {
+  bool value1 = param.asInt();
+  // Check these values and turn the relay1 ON and OFF
+  if (value1 == 1) {
+    digitalWrite(relay1, LOW);
+  } else {
+    digitalWrite(relay1, HIGH);
+  }
+}
+//Get the button values
+BLYNK_WRITE(V2) {
+  bool value2 = param.asInt();
+  // Check these values and turn the relay2 ON and OFF
+  if (value2 == 1) {
+    digitalWrite(relay2, LOW);
+  } else {
+    digitalWrite(relay2, HIGH);
+  }
+}
+void setup()
+{
+  //Set the relay pins as output pins
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+
+  // Turn OFF the relay
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, HIGH);
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
